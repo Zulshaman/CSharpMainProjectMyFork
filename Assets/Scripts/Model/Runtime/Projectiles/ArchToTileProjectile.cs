2dd403b4a2bf9747155dbd120d13e7c02e8a6bc5
Zulshaman <nikolay-archi@yandex.ru>
2026-02-24 10:25:24 +0500

    Домашка 2. Zulshaman

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -29,7 +29,7 @@ namespace Model.Runtime.Projectiles
             float maxHeight = totalDistance * 0.6f;
 
             localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
-            ///////////////////////////////////////
+            //////////////////////////////////////////
 
 
             ///////////////////////////////////////
@@ -43,3 +43,4 @@ namespace Model.Runtime.Projectiles
     }
 
 }
+
